12bc15d385706e77853eb5547f7b435a775d243a
Eddie Rangel <hacker@eddierangel.com>
2015-01-23 13:37:20 -0800

    Add Interfaces for Services

---
+++ BuenaHealth.Core/Services/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuenaHealth.Core.Models;
+
+namespace BuenaHealth.Core.Services
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Demographics> DemographicsRepository { get; }
+        IRepository<Notes> NotesRepository { get; }
+        IRepository<VitalSigns> VitalSignsRepository { get; }
+    }
+}
