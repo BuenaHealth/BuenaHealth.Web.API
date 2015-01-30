c01f60f3e61e4c37439ed0a16fa5401123718a2c
erangel <eddie.rangel@doverals.com>
2015-01-30 11:51:37 -0800

    Creating Stub Repository for Testing

---
+++ BuenaHealth.Infrastructure/Stubs/InMemoryRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using BuenaHealth.Core.Services;
+
+namespace BuenaHealth.Infrastructure.Stubs
+{
+    public class InMemoryRepository<T> : IRepository<T> where T : class
+    {
+        private readonly List<T> _list;
+
+        public InMemoryRepository(List<T> list)
+        {
+            _list = list;
+        }
+
+        public IQueryable<T> AsQueryable()
+        {
+            return _list.AsQueryable();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _list;
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return _list.AsQueryable().Where(predicate);
+        }
+
+        public T Single(Expression<Func<T, bool>> predicate)
+        {
+            return _list.AsQueryable().Single(predicate);
+        }
+
+        public T SingleOrDefault(Expression<Func<T, bool>> predicate)
+        {
+            return _list.AsQueryable().SingleOrDefault(predicate);
+        }
+
+        public T First(Expression<Func<T, bool>> predicate)
+        {
+            return _list.AsQueryable().First(predicate);
+        }
+
+        public T GetById(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T entity)
+        {
+            _list.Add(entity);
+
+        }
+
+        public void Delete(T entity)
+        {
+            _list.Remove(entity);
+        }
+
+        public void Attach(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
