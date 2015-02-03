6ff9f35179c620279b8ed56bf26cb27bc1c0643f
Eddie Rangel <hacker@eddierangel.com>
2015-02-02 20:54:24 -0800

    Refactoring Solution Structure

--- BuenaHealth.Common.Tests/Properties/AssemblyInfo.cs
+++ BuenaHealth.Common.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BuenaHealth.Web.API.Tests")]
+[assembly: AssemblyTitle("BuenaHealth.Common.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BuenaHealth.Web.API.Tests")]
+[assembly: AssemblyProduct("BuenaHealth.Common.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b0ebb63b-f1e3-4747-b660-c33a77f166e4")]
+[assembly: Guid("ba053180-8c17-418e-9f19-b1919afec131")]
 
 // Version information for an assembly consists of the following four values:
 //
