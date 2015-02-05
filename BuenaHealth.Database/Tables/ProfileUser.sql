d92cc77539d7bbbc1d17b13e06e4f3586723dc81
erangel <eddie.rangel@doverals.com>
2015-02-04 16:10:37 -0800

    Add SQL Scripts

---
+++ BuenaHealth.Database/Tables/ProfileUser.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[ProfileUser]
+(
+	[ProfileId] bigint NOT NULL,
+	[UserId] bigint not null,
+	[ts] rowversion not null,
+	primary key (ProfileId, UserId),
+	foreign key (UserId) references dbo.[User] (UserId),
+	foreign key (ProfileId) references dbo.Task (ProfileId) 
+)
+go
+
+create index ix_ProfileUser_UserId on ProfileUser(UserId)
+go
