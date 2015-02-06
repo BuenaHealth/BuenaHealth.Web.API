f3520c55561b6870b7dbdbdc813a3d1c395245d0
Eddie Rangel <hacker@eddierangel.com>
2015-02-06 12:03:34 -0800

    Fixed Typo Fixed Build Settings

--- BuenaHealth.Database/Tables/Demographic.sql
+++ BuenaHealth.Database/Tables/Demographic.sql
@@ -6,9 +6,9 @@
 	[RaceId] BIGINT         NOT NULL,
 	[EthnicityId] BIGINT         NOT NULL,
 	[ts]       rowversion     NOT NULL,
-	PRIMARY KEY CLUSTERED ([DemographicId] ASC)
+	PRIMARY KEY CLUSTERED ([DemographicId] ASC),
 	FOREIGN KEY ([SexId]) REFERENCES [dbo].[Sex] ([SexId]),
 	FOREIGN KEY ([LanguageId]) REFERENCES [dbo].[Language] ([LanguageId]),
-	FOREIGN KEY ([RaceId]) REFERENCES [dbo].[Race] ([RaceId])
+	FOREIGN KEY ([RaceId]) REFERENCES [dbo].[Race] ([RaceId]),
 	FOREIGN KEY ([EthnicityId]) REFERENCES [dbo].[Ethnicity] ([EthnicityId])
 );
