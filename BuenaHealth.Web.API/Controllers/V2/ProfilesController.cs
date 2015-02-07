936b45740df3f806e6c0f5049dd7fe2780f4b1bf
Eddie Rangel <hacker@eddierangel.com>
2015-02-06 21:05:42 -0800

    Implement V2 AddProfile

--- BuenaHealth.Web.API/Controllers/V2/ProfilesController.cs
+++ BuenaHealth.Web.API/Controllers/V2/ProfilesController.cs
@@ -4,10 +4,21 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using BuenaHealth.Web.API.Models;
 
 namespace BuenaHealth.Web.API.Controllers.V2
 {
+    [RoutePrefix("api/{apiVersion:apiVersionConstraint(V2)}/profiles")]
     public class ProfilesController : ApiController
     {
+        [Route("", Name = "AddProfileRouteV2")]
+        [HttpPost]
+        public Profile AddProfile(HttpRequestMessage requestMessage, Models.Profile newProfile)
+        {
+            return new Profile
+            {
+                Name = "In V2, Profile.Name = " + newProfile.Name
+            };
+        }
     }
 }
