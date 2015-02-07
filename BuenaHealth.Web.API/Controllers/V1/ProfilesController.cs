063f58ca7d3c7994e751c0e8066e1976a18fc011
Eddie Rangel <hacker@eddierangel.com>
2015-02-06 20:55:18 -0800

    Implement AddProfile in ProfilesController V1

--- BuenaHealth.Web.API/Controllers/V1/ProfilesController.cs
+++ BuenaHealth.Web.API/Controllers/V1/ProfilesController.cs
@@ -4,10 +4,22 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using BuenaHealth.Web.API.Models;
+using BuenaHealth.Web.Common.Routing;
 
 namespace BuenaHealth.Web.API.Controllers.V1
 {
+    [ApiVersionOneRoutePrefix("profiles")]
     public class ProfilesController : ApiController
     {
+        [Route("",Name = "AddProfileRoute")]
+        [HttpPost]
+        public Profile AddProfile(HttpRequestMessage requestMessage, Models.Profile newProfile)
+        {
+            return new Profile
+            {
+                Name = "In V1, Profile.Name = " + newProfile.Name
+            };
+        }
     }
 }
