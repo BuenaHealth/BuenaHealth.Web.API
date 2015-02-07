1e30bb2c182b6a40107778705672b8e2ace7fef9
Eddie Rangel <hacker@eddierangel.com>
2015-02-07 14:33:36 -0800

    Creating Ninject Config in App Start

---
+++ BuenaHealth.Web.API/App_Start/NinjectConfigurator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BuenaHealth.Common;
+using BuenaHealth.Common.Logging;
+using log4net.Appender;
+using log4net.Config;
+using Ninject;
+
+namespace BuenaHealth.Web.API.App_Start
+{
+    public class NinjectConfigurator
+    {
+        public void Configure(IKernel container)
+        {
+            AddBindings(container);
+        }
+
+        private void AddBindings(IKernel container)
+        {
+            ConfigureLog4Net(container);
+            container.Bind<IDateTime>().To<DateTimeAdapter>().InSingletonScope();
+        }
+
+        private void ConfigureLog4Net(IKernel container)
+        {
+            XmlConfigurator.Configure();
+
+            var logManager = new LogManagerAdapter();
+
+            container.Bind<ILogManager>().ToConstant(logManager);
+
+        }
+    }
+}
