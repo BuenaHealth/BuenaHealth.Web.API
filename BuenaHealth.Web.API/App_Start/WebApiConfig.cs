50d19cec3e67fc36647947a3190b719a6e3fc20c
Eddie Rangel <hacker@eddierangel.com>
2015-02-06 21:44:02 -0800

    Modified WebApiConfig

--- BuenaHealth.Web.API/App_Start/WebApiConfig.cs
+++ BuenaHealth.Web.API/App_Start/WebApiConfig.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using System.Web.Http.Routing;
+using System.Web.Http.ExceptionHandling;
+using BuenaHealth.Web.Common;
+using BuenaHealth.Web.Common.Routing;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
@@ -11,20 +16,17 @@ namespace BuenaHealth.Web.API
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
-        {
+        {  
             // Web API configuration and services
-            // Configure Web API to use only bearer token authentication.
-            config.SuppressDefaultHostAuthentication();
-            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            var contstraintResolver = new DefaultInlineConstraintResolver();
+            contstraintResolver.ConstraintMap.Add("apiVersionContraint", typeof(ApiVersionConstraint));
+
 
             // Web API routes
-            config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            config.MapHttpAttributeRoutes(contstraintResolver);
+            config.Services.Replace(typeof(IHttpControllerSelector), new NamespaceHttpControllerSelector(config));
+        
+           
         }
     }
 }
