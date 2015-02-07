e22f629bff75fb836f4904193044d051dea9dbbe
Eddie Rangel <hacker@eddierangel.com>
2015-02-06 20:42:05 -0800

    Createing Attribute Helper using RoutePrefixAttribute

--- BuenaHealth.Web.Common/Routing/ApiVersionOneRoutePrefixAttribute.cs
+++ BuenaHealth.Web.Common/Routing/ApiVersionOneRoutePrefixAttribute.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace BuenaHealth.Web.Common.Routing
 {
-    class ApiVersionOneRoutePrefixAttribute
+    public class ApiVersionOneRoutePrefixAttribute : RoutePrefixAttribute
     {
+        private const string RouteBase = "api/{apiVersion:apiVersionConstraint(V1)}";
+        private const string PrefixRouteBase = RouteBase + "/";
+
+        /// <summary>
+        /// Takes API Version and creates URI
+        /// </summary>
+        /// <param name="routePrefix">API Version URI</param>
+        public ApiVersionOneRoutePrefixAttribute(string routePrefix)
+            : base(string.IsNullOrWhiteSpace(routePrefix)
+             ? RouteBase : PrefixRouteBase + routePrefix)
+        {
+
+        }
     }
 }
