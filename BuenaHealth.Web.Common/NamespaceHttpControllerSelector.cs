8a1c1ff2da1a1d49dda4ce6ac76c92d54eac4b54
Eddie Rangel <hacker@eddierangel.com>
2015-02-06 21:12:12 -0800

    Creating Custom Http Controller Selector Implement Interface

---
+++ BuenaHealth.Web.Common/NamespaceHttpControllerSelector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dispatcher;
+
+namespace BuenaHealth.Web.Common
+{
+    public class NamespaceHttpControllerSelector : IHttpControllerSelector
+    {
+        public HttpControllerDescriptor SelectController(HttpRequestMessage request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, HttpControllerDescriptor> GetControllerMapping()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
