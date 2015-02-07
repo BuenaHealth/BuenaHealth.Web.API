cb7731988a17e97cd95f9d18be01cf568067625a
Eddie Rangel <hacker@eddierangel.com>
2015-02-06 19:48:05 -0800

    Fixed issue with script. Added ApiVersionContraint

--- BuenaHealth.Database/Scripts/ReferenceData.sql
+++ BuenaHealth.Database/Scripts/ReferenceData.sql
@@ -1,6 +1,6 @@
-﻿﻿if not exists(select * from dbo.Status where Name = 'Not Started')
+﻿if not exists(select * from dbo.[Status] where Name = 'Not Started')
 	insert into dbo.Status(Name, Ordinal) values('Not Started', 0);
-if not exists(select * from dbo.Status where Name = 'In Progress')
+if not exists(select * from dbo.[Status] where Name = 'In Progress')
 	insert into dbo.Status(Name, Ordinal) values('In Progress', 1);
-if not exists(select * from dbo.Status where Name = 'Completed')
+if not exists(select * from dbo.[Status] where Name = 'Completed')
 	insert into dbo.Status(Name, Ordinal) values('Completed', 2);
