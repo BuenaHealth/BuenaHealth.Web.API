5f88b2f44198804de6bc1bab0a067b66818bac15
Eddie Rangel <hacker@eddierangel.com>
2015-02-09 20:58:30 -0800

    Implemented Version Interface on Status Class

--- BuenaHealth.Data/Entities/Status.cs
+++ BuenaHealth.Data/Entities/Status.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BuenaHealth.Data.Interfaces;
 
 namespace BuenaHealth.Data.Entities
 {
-    public class Status
+    public class Status : IVersionedEntity
     {
         public virtual long StatusId { get; set; }
         public virtual string Name { get; set; }
