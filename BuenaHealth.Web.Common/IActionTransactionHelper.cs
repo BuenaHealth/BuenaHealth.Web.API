a3f526073f39ccb12413befa26cb99ed8bb9ee65
Eddie Rangel <hacker@eddierangel.com>
2015-02-10 21:07:10 -0800

    Creating an ActionTransactionHelper and WebContainerManager

---
+++ BuenaHealth.Web.Common/IActionTransactionHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Http.Filters;
+
+namespace BuenaHealth.Web.Common
+{
+    public interface IActionTransactionHelper
+    {
+        void BeginTransaction();
+        void EndTransaction(HttpActionExecutedContext filterContext);
+        void CloseSession();
+    }
+}
