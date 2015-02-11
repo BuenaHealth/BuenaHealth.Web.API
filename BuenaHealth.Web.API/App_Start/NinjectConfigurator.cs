a64b6dd8295de06bb1caea2d6e26d16550d741f9
Eddie Rangel <hacker@eddierangel.com>
2015-02-10 20:53:37 -0800

    Modified NinjectConfigurator. Included ConnectionString for Web Config

--- BuenaHealth.Web.API/App_Start/NinjectConfigurator.cs
+++ BuenaHealth.Web.API/App_Start/NinjectConfigurator.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using BuenaHealth.Common;
+﻿using BuenaHealth.Common;
 using BuenaHealth.Common.Logging;
-using log4net.Appender;
+using BuenaHealth.Data.SqlServer.Mapping;
+using BuenaHealth.Web.Common;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
 using log4net.Config;
+using NHibernate;
+using NHibernate.Context;
 using Ninject;
+using Ninject.Activation;
+using Ninject.Web.Common;
 
 namespace BuenaHealth.Web.API.App_Start
 {
@@ -20,6 +23,7 @@ namespace BuenaHealth.Web.API.App_Start
         private void AddBindings(IKernel container)
         {
             ConfigureLog4Net(container);
+            ConfigureNHibernate(container);
             container.Bind<IDateTime>().To<DateTimeAdapter>().InSingletonScope();
         }
 
@@ -31,6 +35,31 @@ namespace BuenaHealth.Web.API.App_Start
 
             container.Bind<ILogManager>().ToConstant(logManager);
 
+        }
+
+        private void ConfigureNHibernate(IKernel container)
+        {
+            var sessionFactory = Fluently.Configure()
+               .Database(MsSqlConfiguration.MsSql2012.ConnectionString(c => c.FromConnectionStringWithKey("BuenaHealthDb")))
+               .CurrentSessionContext("web")
+               .Mappings(m => m.FluentMappings.AddFromAssemblyOf<ProfileMap>())
+               .BuildSessionFactory();
+
+            container.Bind<ISessionFactory>().ToConstant(sessionFactory);
+            container.Bind<ISession>().ToMethod(CreateSession).InRequestScope();
+            //container.Bind<IActionTransactionHelper>().To<ActionTransactionHelper>().InRequestScope();
+
+        }
+
+        private ISession CreateSession(IContext context)
+        {
+            var sessionFactory = context.Kernel.Get<ISessionFactory>();
+            if (!CurrentSessionContext.HasBind(sessionFactory))
+            {
+                var session = sessionFactory.OpenSession();
+                CurrentSessionContext.Bind(session);
+            }
+            return sessionFactory.GetCurrentSession();
         }
     }
 }
