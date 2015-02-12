71d7a5fbe4c1f9bb5e2ef4757ce0122462af402c
Eddie Rangel <hacker@eddierangel.com>
2015-02-11 21:15:08 -0800

    Removing unused using statements

--- BuenaHealth.Data.Sqlserver/Mapping/UserMap.cs
+++ BuenaHealth.Data.Sqlserver/Mapping/UserMap.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BuenaHealth.Data.Entities;
+﻿using BuenaHealth.Data.Entities;
 
 namespace BuenaHealth.Data.SqlServer.Mapping
 {
--- BuenaHealth.Data/Entities/Status.cs
+++ BuenaHealth.Data/Entities/Status.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BuenaHealth.Data.Interfaces;
+﻿using BuenaHealth.Data.Interfaces;
 
 namespace BuenaHealth.Data.Entities
 {
