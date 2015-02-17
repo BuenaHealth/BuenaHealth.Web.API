16571f9ef6ae6137293af7472f0d73e2718dc190
Eddie Rangel <hacker@eddierangel.com>
2015-02-16 17:10:21 -0800

    Update using reference

--- BuenaHealth.Web.API/AutoMappingConfiguration/ProfileAssociatesResolver.cs
+++ BuenaHealth.Web.API/AutoMappingConfiguration/ProfileAssociatesResolver.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using AutoMapper;
 using BuenaHealth.Common.TypeMapping;
-using BuenaHealth.Data.Entities;
 using BuenaHealth.Web.Common;
+using Profile = BuenaHealth.Data.Entities.Profile;
 using User = BuenaHealth.Web.API.Models.User;
 
 namespace BuenaHealth.Web.API.AutoMappingConfiguration
 {
-    public class ProfileAssociatesResolver : AutoMapper.ValueResolver<Profile, List<User>>
+    public class ProfileAssociatesResolver : ValueResolver<Profile, List<User>>
     {
         public IAutoMapper AutoMapper
         {
