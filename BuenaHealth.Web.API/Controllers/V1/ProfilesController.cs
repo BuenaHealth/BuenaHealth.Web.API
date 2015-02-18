589b34fb7f279695c1a39a2b838f60c4a0073eec
Eddie Rangel <hacker@eddierangel.com>
2015-02-17 22:28:30 -0800

    Modified Controller to include Processor

--- BuenaHealth.Web.API/Controllers/V1/ProfilesController.cs
+++ BuenaHealth.Web.API/Controllers/V1/ProfilesController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Http;
 using System.Web.Http;
+using BuenaHealth.Web.API.MaintenanceProcessing;
 using BuenaHealth.Web.API.Models;
 using BuenaHealth.Web.Common;
 using BuenaHealth.Web.Common.Routing;
@@ -10,14 +11,20 @@ namespace BuenaHealth.Web.API.Controllers.V1
     [UnitOfWorkActionFilter]
     public class ProfilesController : ApiController
     {
+        private readonly IAddProfileMaintenanceProcessor _addProfileMaintenanceProcessor;
+
+        public ProfilesController(IAddProfileMaintenanceProcessor addProfileMaintenanceProcessor)
+        {
+            _addProfileMaintenanceProcessor = addProfileMaintenanceProcessor;
+        }
+
         [Route("",Name = "AddProfileRoute")]
         [System.Web.Http.HttpPost]
         public Profile AddProfile(HttpRequestMessage requestMessage, NewProfile newProfile)
         {
-            return new Profile
-            {
-                Name = "In V1, Profile.Name = " + newProfile.Name
-            };
+            var profile = _addProfileMaintenanceProcessor.AddProfile(newProfile);
+
+            return profile;
         }
     }
 }
