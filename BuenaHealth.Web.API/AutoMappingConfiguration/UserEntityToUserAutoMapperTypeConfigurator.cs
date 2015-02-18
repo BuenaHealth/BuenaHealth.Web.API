cddc7128ab64b51a679fbce18a285d90353a6eb8
erangel <eddie.rangel@doverals.com>
2015-02-17 17:01:39 -0800

    Creating Mapping for User

---
+++ BuenaHealth.Web.API/AutoMappingConfiguration/UserEntityToUserAutoMapperTypeConfigurator.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BuenaHealth.Common.TypeMapping;
+using BuenaHealth.Data.Entities;
+
+namespace BuenaHealth.Web.API.AutoMappingConfiguration
+{
+    public class UserEntityToUserAutoMapperTypeConfigurator : IAutoMapperTypeConfigurator
+    {
+        public void Configure()
+        {
+            Mapper.CreateMap<User, Models.User>()
+                .ForMember(opt => opt.Links, x => x.Ignore());
+        }
+    }
+}
---
+++ BuenaHealth.Web.API/AutoMappingConfiguration/UserToUserEntityAutoMapperTypeConfigurator.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BuenaHealth.Common.TypeMapping;
+using BuenaHealth.Web.API.Models;
+
+namespace BuenaHealth.Web.API.AutoMappingConfiguration
+{
+    public class UserToUserEntityAutoMapperTypeConfigurator : IAutoMapperTypeConfigurator
+    {
+        public void Configure()
+        {
+            Mapper.CreateMap<User, Data.Entities.User>()
+                .ForMember(opt => opt.Version, x => x.Ignore());
+        }
+    }
+}
