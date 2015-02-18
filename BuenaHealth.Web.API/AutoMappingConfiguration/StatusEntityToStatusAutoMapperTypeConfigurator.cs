9517f9b23ea8986da8969f8f9fc28a20d3397087
erangel <eddie.rangel@doverals.com>
2015-02-17 17:12:09 -0800

    Create Status AutoMapper Classses

---
+++ BuenaHealth.Web.API/AutoMappingConfiguration/StatusEntityToStatusAutoMapperTypeConfigurator.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BuenaHealth.Common.TypeMapping;
+using BuenaHealth.Data.Entities;
+
+namespace BuenaHealth.Web.API.AutoMappingConfiguration
+{
+    public class StatusEntityToStatusAutoMapperTypeConfigurator : IAutoMapperTypeConfigurator
+    {
+        public void Configure()
+        {
+            Mapper.CreateMap<Status, Models.Status>();
+        }
+    }
+}
---
+++ BuenaHealth.Web.API/AutoMappingConfiguration/StatusToStatusEntityAutoMapperTypeConfigurator.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BuenaHealth.Common.TypeMapping;
+using BuenaHealth.Web.API.Models;
+
+namespace BuenaHealth.Web.API.AutoMappingConfiguration
+{
+    public class StatusToStatusEntityAutoMapperTypeConfigurator : IAutoMapperTypeConfigurator
+    {
+        public void Configure()
+        {
+            Mapper.CreateMap<Status,Data.Entities.Status>()
+                .ForMember(opt => opt.Version, x => x.Ignore());
+        }
+    }
+}
