8449d6963f4aa7e5dbf04c82fe4abb062215530d
Eddie Rangel <hacker@eddierangel.com>
2015-02-17 22:37:21 -0800

    Adding Status Map

---
+++ BuenaHealth.Data.Sqlserver/Mapping/StatusMap.cs
@@ -0,0 +1,15 @@
+﻿using BuenaHealth.Data.Entities;
+
+namespace BuenaHealth.Data.SqlServer.Mapping
+{
+  public class StatusMap : VersionedClassMap<Status> 
+     { 
+         public StatusMap() 
+         { 
+             Id(x => x.StatusId); 
+             Map(x => x.Name).Not.Nullable(); 
+             Map(x => x.Ordinal).Not.Nullable(); 
+         } 
+     } 
+
+}
