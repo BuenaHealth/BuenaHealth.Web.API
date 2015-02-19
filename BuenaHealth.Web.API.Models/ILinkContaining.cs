2b67d38802b8e6ad17fe9f2d6db50ff6c0c27447
Eddie Rangel <hacker@eddierangel.com>
2015-02-18 21:45:13 -0800

    Modified class. created constants class.

---
+++ BuenaHealth.Web.API.Models/ILinkContaining.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BuenaHealth.Web.API.Models
+{
+    public interface ILinkContaining
+    {
+        List<Link> Links { get; set; }
+        void AddLink(Link link);
+    }
+}
--- BuenaHealth.Web.API.Models/Profile.cs
+++ BuenaHealth.Web.API.Models/Profile.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BuenaHealth.Web.API.Models
 {
-    public class Profile
+    public class Profile : ILinkContaining
     {
         private List<Link> _links;
 
