04f382416d2feb1ad621c126d58a0dad3d2d7b7c
Eddie Rangel <hacker@eddierangel.com>
2015-02-20 11:42:05 -0800

    CreatedLocationLinkCalculator class

---
+++ BuenaHealth.Web.API/MaintenanceProcessing/LocationLinkCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using BuenaHealth.Common;
+using BuenaHealth.Web.API.Models;
+
+namespace BuenaHealth.Web.API.MaintenanceProcessing
+{
+    public static class LocationLinkCalculator
+    {
+        public static Uri GetLocationLink(ILinkContaining linkContaining)
+        {
+            var locationLink = linkContaining.Links.FirstOrDefault(
+                x => x.Rel == Constants.CommonLinkRelValues.Self);
+
+            return locationLink == null ? null : new Uri(locationLink.Href);
+        }
+    }
+}
