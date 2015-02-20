bbcef92736a7c48a38586ff80d90cc7d7c8fc32e
Eddie Rangel <hacker@eddierangel.com>
2015-02-20 11:54:38 -0800

    Created Profile Created Action Result

---
+++ BuenaHealth.Web.API/MaintenanceProcessing/ProfileCreatedActionResult.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using BuenaHealth.Web.API.Models;
+
+namespace BuenaHealth.Web.API.MaintenanceProcessing
+{
+    public class ProfileCreatedActionResult : IHttpActionResult
+    {
+        private readonly Profile _createdProfile;
+        private readonly HttpRequestMessage _requestMessage;
+
+        public ProfileCreatedActionResult(HttpRequestMessage requestMessage, Profile createdProfile)
+        {
+            _requestMessage = requestMessage;
+            _createdProfile = createdProfile;
+        }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return System.Threading.Tasks.Task.FromResult(Execute());
+        }
+
+        public HttpResponseMessage Execute()
+        {
+            var responseMessage = _requestMessage.CreateResponse(HttpStatusCode.Created, _createdProfile);
+            responseMessage.Headers.Location = LocationLinkCalculator.GetLocationLink(_createdProfile);
+
+            return responseMessage;
+        }
+    }
+}
