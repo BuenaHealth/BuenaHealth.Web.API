579bc83bac2234bddd1b57a8866366461d52acdc
erangel <eddie.rangel@doverals.com>
2015-03-11 14:50:06 -0700

    Implemented Profile Workflow Controller

---
+++ BuenaHealth.Web.API/Controllers/V1/ProfileWorkflowController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using BuenaHealth.Web.API.MaintenanceProcessing;
+using BuenaHealth.Web.API.Models;
+
+namespace BuenaHealth.Web.API.Controllers.V1
+{
+    public class ProfileWorkflowController : ApiController
+    {
+        private readonly IStartProfileWorkflowProcessor _startStartProfileWorkflowProcessor;
+
+        public ProfileWorkflowController(IStartProfileWorkflowProcessor startProfileWorkflowProcessor)
+        {
+            _startStartProfileWorkflowProcessor = startProfileWorkflowProcessor;
+        }
+
+        public Profile StartProfile(long profileId)
+        {
+            var profile = _startStartProfileWorkflowProcessor.StartProfile(profileId);
+            return profile;
+        }
+
+    }
+}
