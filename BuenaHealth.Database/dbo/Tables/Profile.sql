a072d02e43b0d7e6b6365f82cbca557f1376575f
Eddie Rangel <hacker@eddierangel.com>
2015-03-17 21:11:20 -0700

    Restructuring SQL Project

---
+++ BuenaHealth.Database/dbo/Tables/Profile.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Profile] (
+	[ProfileId]        BIGINT         IDENTITY (1, 1) NOT NULL,
+	[Name]       NVARCHAR (100) NOT NULL,    
+	[StatusId]      BIGINT         NOT NULL,
+	[CreatedDate]   DATETIME2 (7)  NOT NULL,
+	[StartDate]	    DATETIME2(7) NOT NULL,
+	[CreatedUserId]   bigint  NOT NULL,
+	[ts]            rowversion     NOT NULL,
+	PRIMARY KEY CLUSTERED ([ProfileId] ASC),
+	FOREIGN KEY ([StatusId]) REFERENCES [dbo].[Status] ([StatusId]),
+	FOREIGN KEY ([CreatedUserId]) REFERENCES [dbo].[User] ([UserId])
+);
